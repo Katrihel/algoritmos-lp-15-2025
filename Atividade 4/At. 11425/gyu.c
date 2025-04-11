877649f8d834862bf2b78b1c2ef0704a3d831764
Katrihel <eduardo.delmondesr@gmail.com>
2025-04-11 12:36:16 -0300

    commit

---
+++ Atividade 4/At. 11425/gyu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv []){
+
+    float notas[] = (9.7, 8.0, 10.0, 5,6);
+
+    for(int i = 0; i < 4; i++){
+        printf("Nota[%d]");
+    }
+        return 0;
+}
